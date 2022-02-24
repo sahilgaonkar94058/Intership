52404dde73c82d345bf73a43de73ccdc8e669512
sahilgaonkar94058 <84930040+sahilgaonkar94058@users.noreply.github.com>
2022-02-24 14:52:56 +0530

    Add files via upload

---
+++ build/precache-manifest.4735a60326002b410413acdac7d84bbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c67595a0f4a9333b35a81cef8e388f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e2ebecbc8299b7945cf",
+    "url": "/static/css/main.40c592de.chunk.css"
+  },
+  {
+    "revision": "ad3011d31ddfa19666e4",
+    "url": "/static/js/2.a2dde473.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a2dde473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e2ebecbc8299b7945cf",
+    "url": "/static/js/main.0d5a05b3.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
